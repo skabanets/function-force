c6386ce0810b69580aa01b453e860565fecae3d6
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-12-19 21:24:24 +0200

    corrected

--- src/js/components/discount-products.js
+++ src/js/components/discount-products.js
@@ -18,7 +18,7 @@ function renderDiscountItems(discounts) {
       <div class="discount-order">
         <h3 class="discount-name">${discount.name}</h3>
         <p class="discount-price">&#36;${discount.price}</p>
-        <button type="" class="discount-button">
+        <button type="button" class="discount-button">
           <svg class="discount-shopping-icon" width="18" height="18">
             <use href="./images/sprite.svg#icon-shopping-cart"></use>
           </svg>
