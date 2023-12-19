ce0e3a90cc4fdef36e8bd83fecacfb40aa01075b
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-12-19 18:08:26 +0200

    done html/css + js

--- src/js/components/discount-products.js
+++ src/js/components/discount-products.js
@@ -1,20 +1,23 @@
-<div class="discount-container container">
-  <h2 class="discount-title">Discount products</h2>
-  <ul class="discount-list" id="list">
+function renderDiscountItems(discounts) {
+  const discountList = document.getElementById('list');
+
+  const itemsHTML = discounts
+    .map(
+      discount => `
     <li class="discount-item">
       <svg class="discount-icon" width="60" height="60">
         <use href="./images/sprite.svg#icon-discount"></use>
       </svg>
       <div class="discount-container-img">
         <img
-          src="./images/discount-img.jpg"
-          alt="discount-img"
+          src="${discount.imgSrc}"
+          alt="${discount.name}-img"
           class="discount-img"
         />
       </div>
       <div class="discount-order">
-        <h3 class="discount-name">Apricot</h3>
-        <p class="discount-price">&#36;0.79</p>
+        <h3 class="discount-name">${discount.name}</h3>
+        <p class="discount-price">&#36;${discount.price}</p>
         <button type="" class="discount-button">
           <svg class="discount-shopping-icon" width="18" height="18">
             <use href="./images/sprite.svg#icon-shopping-cart"></use>
@@ -22,5 +25,11 @@
         </button>
       </div>
     </li>
-  </ul>
-</div>
+  `
+    )
+    .join('');
+
+  discountList.insertAdjacentHTML('beforeend', itemsHTML);
+}
+
+renderDiscountItems(discountsData);
