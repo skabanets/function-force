38bb2c2d5acfd154fdcae7f75d2aac74f5110867
olga-deputat <144669008+olga-deputat@users.noreply.github.com>
2023-12-20 22:46:43 +0200

    update js

--- src/js/components/filters.js
+++ src/js/components/filters.js
@@ -1,34 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dropdowns = document.querySelectorAll(".dropdown-filter");
-  const caretIcons = document.querySelectorAll(".form-click-filter");
-  const menuFilters = document.querySelectorAll(".menu-filter");
-
-  caretIcons.forEach(function (icon, index) {
-    icon.addEventListener("click", function () {
-      menuFilters.forEach(function (menu, i) {
-        if (i !== index) {
-          menu.classList.remove("menu-open");
-        }
-      });
-
-      menuFilters[index].classList.toggle("menu-open");
-    });
-  });
-
-  menuFilters.forEach(function (menu) {
-    const menuItems = menu.querySelectorAll("li");
-    menuItems.forEach(function (item) {
-      item.addEventListener("click", function () {
-        menu.classList.remove("menu-open");
-      });
-    });
-  });
-
-  document.addEventListener("click", function (event) {
-    if (!event.target.closest(".dropdown-filter")) {
-      menuFilters.forEach(function (menu) {
-        menu.classList.remove("menu-open");
-      });
-    }
-  });
-});
+const dropdown = document.querySelectorAll('.dropdown-filter');
+function toggleClass(target) {
+  const drFilter = target.closest('.dropdown-filter');
+  console.log(drFilter);
+  drFilter.childNodes[5].classList.toggle("menu-open");
+}
+dropdown.forEach(item => {
+item.addEventListener('click', (e) => toggleClass(e.target))
+}
+  )
