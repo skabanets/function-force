8dd11a5410c4d9173837e1760254aec64ccabbd0
Zhurbey Artem <zhurbeyartem@gmail.com>
2023-12-20 15:56:09 +0200

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,83 @@
 import { getProducts } from './api';
+import { getItem, setItem } from './storage';
 
 const list = document.querySelector('.js-cart-list');
 
 const cards = async () => {
   try {
-    let limit = 6;
+    let limit = 10;
     const width = window.innerWidth;
-    if (width >= 768 && width < 1440) limit = 8;
-    else if (width >= 1440) limit = 9;
+    // if (width >= 768 && width < 1440) limit = 8;
+    // else if (width >= 1440) limit = 9;
 
     list.innerHTML = '';
     const { results } = await getProducts({
-      page: 1,
+      page: 25,
       sort: {
         field: 'byABC',
         value: true,
       },
       limit,
     });
+    const bucket = getItem('bucket');
+
     const res = results.map(
       el =>
         `
-  <li class="cart">
+  <li class="cart" data-id="${el._id}">
   <div class="cart-container">
   <div class="cart-img-container">
     <img src="${el.img}" alt="" class="cart-img">
+    ${
+      el.is10PercentOff
+        ? `<svg class="discount-svg" width="60" height="60">
+              <use
+                href="/sprite.f14d31f7.svg#icon-discount"
+              ></use>
+            </svg>`
+        : ''
+    }
   </div>
     <p class="cart-name">${el.name}</p>
     <ul class="info">
-      <li class="info-item">Category: <span class="info-text">${el.category}</span></li>
+      <li class="info-item">Category: <span class="info-text">${
+        el.category
+      }</span></li>
       <li class="info-item">Size: <span class="info-text">${el.size}</span></li>
-      <li class="info-item">Popularity: <span class="info-text">${el.popularity}</span></li>
+      <li class="info-item">Popularity: <span class="info-text">${
+        el.popularity
+      }</span></li>
     </ul>
     <div class="buy-info">
       <p class="buy-price">$${el.price}</p>
-      <button class="buy" type="button">
-        <svg class="buy-svg" width="18" height="18">
-          <use href="/sprite.f14d31f7.svg#icon-shopping-cart"></use>
-        </svg>
-      </button>
+    
+      ${
+        bucket.some(item => item.id === el._id)
+          ? `<button class="check" type="button" disabled>
+            <svg class="buy-svg" width="18" height="18">
+              <use
+                class="check-svg"
+                href="/sprite.f14d31f7.svg#icon-check"
+              ></use>
+            </svg>
+          </button>`
+          : `<button class="buy buy-btn" type="button">
+            <svg class="buy-svg buy-btn" width="18" height="18">
+              <use
+                class="buy-btn"
+                href="/sprite.f14d31f7.svg#icon-shopping-cart"
+              ></use>
+            </svg>
+          </button>`
+      }
+      <button class="check hidden" type="button">
+            <svg class="buy-svg " width="18" height="18">
+              <use
+                class="buy-btn"
+                href="/sprite.f14d31f7.svg#icon-check"
+              ></use>
+            </svg>
+          </button>
     </div>
     </div>
   </li>
@@ -52,3 +91,20 @@ const cards = async () => {
 };
 
 cards();
+
+const buyItem = target => {
+  if (!target.classList.contains('buy-btn')) return;
+  const id = target.closest('.cart').dataset.id;
+  const data = {
+    id,
+    amount: 1,
+  };
+  const bucket = getItem('bucket');
+  setItem('bucket', [...bucket, data]);
+  const buyBtn = target.closest('.buy');
+  const checkBtn = target.closest('.buy-info').lastElementChild;
+  buyBtn.classList.toggle('hidden');
+  checkBtn.classList.toggle('hidden');
+};
+
+list.addEventListener('click', e => buyItem(e.target));
