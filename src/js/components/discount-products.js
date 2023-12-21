d36f95443b84e5e2dd33891f4adcad5c6c42c0c4
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-12-21 22:32:41 +0200

    done task js

--- src/js/components/discount-products.js
+++ src/js/components/discount-products.js
@@ -1,35 +1,43 @@
-function renderDiscountItems(discounts) {
+async function renderDiscountItems(discounts) {
   const discountList = document.getElementById('list');
 
-  const itemsHTML = discounts
-    .map(
-      discount => `
-    <li class="discount-item">
-      <svg class="discount-icon" width="60" height="60">
-        <use href="./images/sprite.svg#icon-discount"></use>
-      </svg>
-      <div class="discount-container-img">
-        <img
-          src="${discount.imgSrc}"
-          alt="${discount.name}-img"
-          class="discount-img"
-        />
-      </div>
-      <div class="discount-order">
-        <h3 class="discount-name">${discount.name}</h3>
-        <p class="discount-price">&#36;${discount.price}</p>
-        <button type="button" class="discount-button">
-          <svg class="discount-shopping-icon" width="18" height="18">
-            <use href="./images/sprite.svg#icon-shopping-cart"></use>
-          </svg>
-        </button>
-      </div>
-    </li>
-  `
-    )
-    .join('');
+  try {
+    const categories = await getProductsCategories();
+    const itemsHTML = discounts
+      .slice(0, 2)
+      .map(
+        discount => `
+      <li class="discount-item">
+        <svg class="discount-icon" width="60" height="60">
+          <use href="./images/sprite.svg#icon-discount"></use>
+        </svg>
+        <div class="discount-container-img">
+          <img
+            src="${discount.imgSrc}"
+            alt="${discount.name}-img"
+            class="discount-img"
+          />
+        </div>
+        <div class="discount-order">
+          <h3 class="discount-name">${discount.name}</h3>
+          <p class="discount-price">&#36;${discount.price}</p>
+          <button type="button" class="discount-button">
+            <svg class="discount-shopping-icon" width="18" height="18">
+              <use href="./images/sprite.svg#icon-shopping-cart"></use>
+            </svg>
+          </button>
+        </div>
+        <div class="product-categories">
+          <p>${categories.join(', ')}</p>
+        </div>
+      </li>
+    `
+      )
+      .join('');
 
-  discountList.insertAdjacentHTML('beforeend', itemsHTML);
+    discountList.insertAdjacentHTML('beforeend', itemsHTML);
+  } catch (error) {
+    console.error('Error rendering discount items:', error);
+  }
 }
-
-renderDiscountItems(discountsData);
+renderDiscountItems(discounts);
