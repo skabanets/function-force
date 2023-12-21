e71ff67e9c591c32e10f15aeebc6d88a8edbee07
Irinassha <irinauash@gmail.com>
2023-12-21 17:05:44 +0000

    menu-categories

--- src/js/components/categories-products.js
+++ src/js/components/categories-products.js
@@ -3,17 +3,24 @@ import { getProductsCategories } from '../api'
 const menuCategories = document.querySelector('.menu-categories-js');
 const categories = document.querySelector('.categories-js');
 
-const selectProducts = arr => {
+const getMarkup = arr => {
     return arr.map(arr => {
         return `<li class='menu-filter-item' data-value='${arr}'>${arr}</li>`;
     }).join('');
 }
 
-async function productsName() {
-    menuCategories.innerHTML = '';
-    const data =  await getProductsCategories();
-    const menu = selectProducts(data)
-    menuCategories.insertAdjacentHTML('beforeend', menu)
+async function renderCategoriesList() {
+    let markup = '';
+    try {
+        menuCategories.innerHTML = '';
+        const data = await getProductsCategories();
+        markup = getMarkup(data);
+        markup = markup + '<li class="show-all-categories">Show all</li>';       
+    } catch(error) {
+        markup = '<li>Error! Try again</li>';
+        // menuCategories.remove();
+    }
 
+   menuCategories.innerHTML = markup;
 } 
- categories.addEventListener('click', productsName)
+categories.addEventListener('click', renderCategoriesList);
