5e778f1e6eb1b68b6f55c7720d1336da0baef71f
skabanets <79872707+skabanets@users.noreply.github.com>
2023-12-21 11:04:19 +0000

    Deploying to gh-pages from @ skabanets/function-force@afb7665a29cd90bb29480c14156d3d6da9420301 🚀

---
+++ index.083656cf.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden-success")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.083656cf.js.map
---
+++ index.72c1bc22.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden-success")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.72c1bc22.js.map
