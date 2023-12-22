b8151b43c911675c0dd3ff2a10af17bee37ac89e
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2023-12-22 09:28:39 +0100

    pre-version

---
+++ src/js/components/modal-markup.js
@@ -0,0 +1,32 @@
+export function getMarkup(product) {
+  return `
+    
+ 
+
+   
+      
+
+      
+        <h2 class="modal-title">${product.name}</h2>
+        <div class="modal-box-info">
+          <p class="classification">
+            Category:<span class="modal-info">${product.category}</span>
+          </p>
+          <p class="classification">
+            Size:<span class="modal-info">${product.size}</span>
+          </p>
+          <p class="classification">
+            Popularity:<span class="modal-info">${product.popularity}</span>
+          </p>
+        </div>
+
+        <p class="modal-description">
+        ${product.desc}
+        </p>
+     
+
+
+  
+    
+    `;
+}
