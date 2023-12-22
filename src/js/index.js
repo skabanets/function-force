f07f75cdafea17b0af5962908f89f984c47b8471
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-22 17:43:56 +0200

    popular-product-js and css

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import './components/product-list';
 import './components/filters';
+import './components/popular-products';
 import './components/discount-products';
 import './components/pagination';
